95cb02b829ee0f098879500af875621e2c510481
Anurag Maurya <76918434+Anuragmaurya-code@users.noreply.github.com>
2023-01-22 00:55:00 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//in react
+ReactDOM.render(
+  <div>
+    <h1>Anurag</h1>
+    <p>this is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
+//int normal js
+// document.getElementById("root").innerHTML = "<h1>" + "what" + "</h1>";
